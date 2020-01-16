d6ef058f69bf7bc3141ee05d2225e6216c8545a4
WLF <wendreolf@gmail.com>
2020-01-16 21:01:47 -0200

    Working with lists to add some actors and others informations

--- src/test/java/pages/MoviePage.java
+++ src/test/java/pages/MoviePage.java
@@ -1,5 +1,10 @@
 package pages;
 
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.Keys;
+
+import java.util.List;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
@@ -11,10 +16,25 @@ public class MoviePage {
         return this;
     }
 
-    public MoviePage create(String title, String status){
+    public MoviePage create(String title, String status, String year, String realeaseDate, List<String> cast, String plot){
         $("input[name=title]").setValue(title);
         this.selectStatus(status);
+        $("input[name=year]").setValue(year);
+        $("input[name=release_date]").setValue(realeaseDate);
+        this.inputCast(cast);
+        $("textarea[name=overview]").setValue(plot);
+
         return this;
+    }
+
+    private void inputCast(List<String> cast){
+
+        SelenideElement element = $(".cast");
+
+        for(String actor : cast){
+            element.setValue(actor);
+            element.sendKeys(Keys.TAB);
+        }
     }
 
     private void selectStatus(String status){
--- src/test/java/tests/MovieTests.java
+++ src/test/java/tests/MovieTests.java
@@ -4,6 +4,9 @@ import commom.BaseTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static com.codeborne.selenide.Condition.text;
 
 public class MovieTests extends BaseTest {
@@ -21,7 +24,12 @@ public class MovieTests extends BaseTest {
     public void shouldRegisterANewMovie() {
         String title = "Jumanji - Next Leve";
         String status = "Pré-Venda";
+        String year = "2020";
+        String releaseDate = "16/01/2020";
+        List<String> cast = Arrays.asList("The Rock", "Jack Black", "Kevin Hart", "Karen Gillan", "Danny DeVito");
+        String plot = "Tentatado a revisitar o mundo de Jumanji, Spencer decide consertar " +
+                " o bug no jogo do game que permite que sejam transportados ao local";
 
-        movie.add().create(title, status);
+        movie.add().create(title, status, year, releaseDate, cast, plot);
     }
 }
